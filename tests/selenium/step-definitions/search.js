7eb7051d0591d757da2edf821e384122b89b53ac
Benjamin Berglund <benjaminberglund@gmail.com>
2020-05-26 00:38:12 +0200

    added ares reporter and json report files for selenium, unit and postman, and added postman tests to npm run and organised report json files into report folder

--- tests/selenium/step-definitions/search.js
+++ tests/selenium/step-definitions/search.js
@@ -5,10 +5,22 @@ module.exports = function() {
   this.Given(/^that I'm at the search page$/, async function () {
     await helpers.loadPage('http://localhost:8080');
     await sleep(2000)
-    let elem = await $('nav>a>h2')
+    let elem = await $('nav>a>h1')
     assert(elem, 'Expected to find h1 element')
     await sleep(2000)
   });
+
+
+  this.When(/^I enter search text "([^"]*)"$/, async function (searchText) {
+    let elem = await $('#search')
+    await elem.sendKeys(searchText)
+  });
+
+  this.Then(/^the first test result should contain the word "([^"]*)"$/, async function (expectedText) {
+    let elem = await $('.product:first-child h2')
+    let text = await elem.getText()
+    assert.include(text, expectedText);
+  })
 
 
 
